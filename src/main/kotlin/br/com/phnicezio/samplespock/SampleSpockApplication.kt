414b6a4c6fd030fd03dfd6d73c1eb5f7305544e1
Paulo Henrique Nicezio Alves <palves@uolinc.com>
2019-02-19 11:34:55 -0300

    initial commit

---
+++ src/main/kotlin/br/com/phnicezio/samplespock/SampleSpockApplication.kt
@@ -0,0 +1,11 @@
+package br.com.phnicezio.samplespock
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SampleSpockApplication
+
+fun main(args: Array<String>) {
+    runApplication<SampleSpockApplication>(*args)
+}
